f3a30233150e3606c84b7d937b3e7365d55396d8
ha-jinwoo <gkwlsdn95@naver.com>
2022-06-16 10:04:15 +0900

    RestTemplate 사용

--- userservice/src/main/java/com/example/userservice/controller/UserController.java
+++ userservice/src/main/java/com/example/userservice/controller/UserController.java
@@ -34,11 +34,11 @@ public class UserController {
 
     @GetMapping("/health_check")
     public String status() {
-        return String.format("It's Working in User Service"
-                 + ", port(local.server.port)=" + env.getProperty("local.server.port")
-                 + ", port(server.port)=" + env.getProperty("server.port")
-                 + ", token secret=" + env.getProperty("token.secret")
-                 + ", token expiration time=" + env.getProperty("token.expiration_time"));
+        return "It's Working in User Service"
+                + ", port(local.server.port)=" + env.getProperty("local.server.port")
+                + ", port(server.port)=" + env.getProperty("server.port")
+                + ", token secret=" + env.getProperty("token.secret")
+                + ", token expiration time=" + env.getProperty("token.expiration_time");
     }
 
     @GetMapping("/welcome")
@@ -73,6 +73,7 @@ public class UserController {
     public ResponseEntity<ResponseUser> getUser(@PathVariable String userId) {
         UserDto userDto = userService.getUserByUserId(userId);
         ResponseUser responseUser = new ModelMapper().map(userDto, ResponseUser.class);
+        System.out.println(responseUser);
         return ResponseEntity.status(HttpStatus.OK).body(responseUser);
     }
 }
