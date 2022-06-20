c764001f8d7025d7be416c33dd6fba9b34909cac
ha-jinwoo <gkwlsdn95@naver.com>
2022-06-20 10:50:37 +0900

    Users Microservice에 CircuitBreaker 적용

--- userservice/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ userservice/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -10,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
@@ -35,6 +37,7 @@ public class UserServiceImpl implements UserService {
     private final RestTemplate restTemplate;
     private final Environment environment;
     private final OrderServiceClient orderServiceClient;
+    private final CircuitBreakerFactory circuitBreakerFactory;
 
     @Override
     public UserDto createUser(UserDto userDto) {
@@ -73,7 +76,12 @@ public class UserServiceImpl implements UserService {
 //        } catch (FeignException ex) {
 //            log.error(ex.getMessage());
 //        }
-        List<ResponseOrder> orders = orderServiceClient.getOrders(userId);
+        /* ErrorDecoder */
+//        List<ResponseOrder> orders = orderServiceClient.getOrders(userId);
+        CircuitBreaker circuitbreaker = circuitBreakerFactory.create("circuitbreaker");
+        List<ResponseOrder> orders = circuitbreaker.run(() -> orderServiceClient.getOrders(userId),
+                throwable -> new ArrayList<>());
+
         userDto.setOrders(orders);
 
         return userDto;
